7ed66a409705a13eae0dde31dbe62a701582db73
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2022-05-20 15:56:58 -0400

    Update plot_Kload_sim.m

--- plot_Kload_sim.m
+++ plot_Kload_sim.m
@@ -9,15 +9,15 @@ t4 = T{4};
 %disp(days)
 
 % color options
-c1= [0.6350 0.0780 0.1840]; % maroon
+c1= [0.3010 0.7450 0.9330]; % blue 
 c2 = [0.4660 0.6740 0.1880]; %green
-c3 = [0.3010 0.7450 0.9330]; % blue
+c3 = [0.6350 0.0780 0.1840]; % maroon
 c4 = [0.4940 0.1840 0.5560]; % purple
 
 % fontsizes
 f.title = 17;
-f.xlabel = 14;
-f.ylabel = 14;
+f.xlabel = 12;
+f.ylabel = 12;
 f.legend = 12;
 f.ticks = 16;
 mark_size1 = 7;
@@ -54,7 +54,7 @@ if plt_means
     for ii = 1:length(T{4})
         [PhiKin_vals4(ii), ~] = get_PhiKin(T{4}(ii), 0, params{4}, Kin_opts{4}, MealInfo{4});
     end %for ii
-    s = subplot(3,1,1);
+    s = subplot(4,1,1);
 %     sum_vals = zeros(days,4);
 %     for i = 1:days
 %         start_id = find(t1 == (i-1)*1440+1);
@@ -75,16 +75,18 @@ if plt_means
     totalPhiKin = MealInfo{1}.K_amount*3*ones(14,1);
     totalPhiKin(3:6) = 400;
     %disp(totalPhiKin)
-    plot(s, totalPhiKin, 'linewidth', lw, 'color', 'k')
+    plot(s, totalPhiKin, 'linewidth', lw, 'color', 'k', 'marker', '.', 'markersize', 15)
     %bar(s, totalPhiKin, 'FaceColor', 'k')
     hold on
     xlabel('time (days)', 'fontsize', f.xlabel)
-    ylabel('\Phi_{Kin} (mEq/day)', 'fontsize', f.ylabel)
+    ylabel('total \Phi_{Kin} (mEq/day)', 'fontsize', f.ylabel)
     ylim([0,450])
     xlim([1,14])
     hold off
+    
+    
     % plasma [K]
-    s = subplot(3,1,2);
+    s = subplot(4,1,2);
     varnum = 5;
     vals1 = X{1}(:,varnum);
     vals2 = X{2}(:,varnum);
@@ -121,21 +123,21 @@ if plt_means
     %plot(s, min_vals(:,1), 'color', c1, 'marker', m1, 'linestyle', 'none');
     plot(s, mean_vals(:,2), 'Linewidth', lw, 'color', c2, 'marker', m2, 'markersize', mark_size1);
     plot(s, max_vals(:,2), 'color', c2, 'marker', m2, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
-    plot(s, mean_vals(:,3), 'Linewidth', lw, 'color', c3, 'marker', m3);
-    plot(s, max_vals(:,3), 'color', c3, 'marker', m3, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
+    %plot(s, mean_vals(:,3), 'Linewidth', lw, 'color', c3, 'marker', m3);
+    %plot(s, max_vals(:,3), 'color', c3, 'marker', m3, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
     plot(s, mean_vals(:,4), 'Linewidth', lw, 'color', c4, 'marker', m4);
     plot(s, max_vals(:,4),  'color', c4, 'marker', m4, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
     lightGrey2   = [0.7 0.7 0.7];
     yline(5.0, 'Linewidth', 1.5, 'color', lightGrey2, 'linestyle', '-');
     yline(3.5, 'Linewidth', 1.5, 'color', lightGrey2, 'linestyle', '-');
     xlabel('time (days)', 'fontsize', f.xlabel)
-    ylabel('plasma [K^+] (mEq/L)', 'fontsize', f.ylabel)
+    ylabel('K_{plasma} (mEq/L)', 'fontsize', f.ylabel)
     xlim([1,14])
     hold off
     
 
     % intracellular [K]
-    s = subplot(3,1,3);
+    s = subplot(4,1,3);
     varnum = 8;
     vals1 = X{1}(:,varnum);
     vals2 = X{2}(:,varnum);
@@ -167,21 +169,67 @@ if plt_means
     plot(s, mean_vals(:,1), 'Linewidth', lw, 'color', c1, 'marker', m1);
     hold on
     plot(s, mean_vals(:,2), 'Linewidth', lw, 'color', c2, 'marker', m2, 'markersize', mark_size1);
-    plot(s, mean_vals(:,3), 'Linewidth', lw, 'color', c3, 'marker', m3);
+    %plot(s, mean_vals(:,3), 'Linewidth', lw, 'color', c3, 'marker', m3);
     plot(s, mean_vals(:,4), 'Linewidth', lw, 'color', c4, 'marker', m4);
     plot(s, max_vals(:,1), 'color', c1, 'marker', m1, 'linewidth', lw, 'linestyle', ':', 'markersize', mark_size1);
     plot(s, max_vals(:,2), 'color', c2, 'marker', m2, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
-    plot(s, max_vals(:,3), 'color', c3, 'marker', m3, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
+    %plot(s, max_vals(:,3), 'color', c3, 'marker', m3, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
     plot(s, max_vals(:,4),  'color', c4, 'marker', m4, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
     %yline(120, 'Linewidth', 1.5, 'color', lightGrey2, 'linestyle', '-');
     yline(140, 'Linewidth', 1.5, 'color', lightGrey2, 'linestyle', '-');
     xlabel('time (days)', 'fontsize', f.xlabel)
-    ylabel('intracellular [K^+] (mEq/L)', 'fontsize', f.ylabel)
+    ylabel('K_{IC}(mEq/L)', 'fontsize', f.ylabel)
     xlim([1,14])
     ylim([125, 155])
     
     hold off
-   legend(labels{1}, labels{2}, labels{3}, labels{4}, 'fontsize', f.legend)
+    
+        % urinary K
+    s = subplot(4,1,4);
+    varnum = 28;
+    vals1 = X{1}(:,varnum);
+    vals2 = X{2}(:,varnum);
+    vals3 = X{3}(:,varnum);
+    vals4 = X{4}(:,varnum);
+    mean_vals = zeros(days,4);
+    for i = 1:days
+        start_id = find(t1 == (i-1)*1440+1);
+        end_id = find(t1 == i*1440);
+        mean_vals(i,1) = mean(vals1(start_id:end_id));
+        max_vals(i,1) = max(vals1(start_id:end_id));
+        min_vals(i,1) = min(vals1(start_id:end_id));
+        start_id = find(t2 == (i-1)*1440+1);
+        end_id = find(t2 == i*1440);
+        mean_vals(i,2) = mean(vals2(start_id:end_id));
+        max_vals(i,2) = max(vals2(start_id:end_id));
+        min_vals(i,2) = min(vals2(start_id:end_id));
+        start_id = find(t3 == (i-1)*1440+1);
+        end_id = find(t3 == i*1440);
+        mean_vals(i,3) = mean(vals3(start_id:end_id));
+        max_vals(i,3) = max(vals3(start_id:end_id));
+        min_vals(i,3) = min(vals3(start_id:end_id));
+        start_id = find(t4 == (i-1)*1440+1);
+        end_id = find(t4 == i*1440);
+        mean_vals(i,4) = mean(vals4(start_id:end_id));
+        max_vals(i,4) = max(vals4(start_id:end_id));
+        min_vals(i,4) = min(vals4(start_id:end_id));
+    end
+    plot(s, mean_vals(:,1), 'Linewidth', lw, 'color', c1, 'marker', m1);
+    hold on
+    plot(s, mean_vals(:,2), 'Linewidth', lw, 'color', c2, 'marker', m2, 'markersize', mark_size1);
+    %plot(s, mean_vals(:,3), 'Linewidth', lw, 'color', c3, 'marker', m3);
+    plot(s, mean_vals(:,4), 'Linewidth', lw, 'color', c4, 'marker', m4);
+    plot(s, max_vals(:,1), 'color', c1, 'marker', m1, 'linewidth', lw, 'linestyle', ':', 'markersize', mark_size1);
+    plot(s, max_vals(:,2), 'color', c2, 'marker', m2, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
+    %plot(s, max_vals(:,3), 'color', c3, 'marker', m3, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
+    plot(s, max_vals(:,4),  'color', c4, 'marker', m4, 'linestyle', ':', 'linewidth', lw, 'markersize', mark_size1);
+    xlabel('time (days)', 'fontsize', f.xlabel)
+    ylabel('\Phi_{uK} (mEq/min)', 'fontsize', f.ylabel)
+    xlim([1,14])
+    ylim([0,1.05])
+    hold off
+   %legend(labels{1}, labels{2}, labels{3}, labels{4}, 'fontsize', f.legend)
+   legend(labels{1}, labels{2}, labels{4}, 'fontsize', f.legend)
     
     
 end % plt_means
